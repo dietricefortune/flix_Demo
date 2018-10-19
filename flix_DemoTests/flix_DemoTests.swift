1c747fc0c29fa76cf9bbd80b439cf3276951cef9
Dietrice Fortune <dietricefortune@dietrices-macbook-pro.local>
2018-10-18 22:14:01 -0400

    Initial Commit

---
+++ flix_DemoTests/flix_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  flix_DemoTests.swift
+//  flix_DemoTests
+//
+//  Created by Dietrice Fortune on 10/18/18.
+//  Copyright © 2018 Dietrice Fortune. All rights reserved.
+//
+
+import XCTest
+@testable import flix_Demo
+
+class flix_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
